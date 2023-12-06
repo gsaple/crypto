17c1a8e0cb4a31c7821ae9898737eb12b0c89283
gsaple <yymelau@gmail.com>
2023-12-06 23:52:29 +1100

    waiting for server component
    waiting for server component

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -27,25 +27,34 @@ function App() {
   }, []);
 
   return (
-    <div className="mt-10 flex items-center justify-center whitespace-nowrap min-w-max">
+    <>
       {data ? (
-        <div>
-          <Header
-            marketCap={data.headerSummary.marketCap}
-            hoursDiff24={data.headerSummary.hoursDiff24}
-          />
-          <Table cryptoData={data.cryptoData} />
+        <div className="mt-10 flex items-center justify-center whitespace-nowrap min-w-max mx-6">
+          <div>
+            <Header
+              marketCap={data.headerSummary.marketCap}
+              hoursDiff24={data.headerSummary.hoursDiff24}
+            />
+            <Table cryptoData={data.cryptoData} />
+          </div>
         </div>
       ) : (
-        <div className="flex items-center space-x-4">
-          <Skeleton className="h-12 w-12 rounded-full bg-gray-500" />
-          <div className="space-y-2">
-            <Skeleton className="h-4 w-[250px] bg-gray-500" />
-            <Skeleton className="h-4 w-[200px] bg-gray-500" />
+        <div className="flex items-center h-screen mx-2">
+          <div className="mx-auto">
+            <p className="text-center font-semibold text-2xl mb-4">
+              waiting for the server to wake up ...
+            </p>
+            <div className="flex items-center space-x-4">
+              <Skeleton className="h-12 w-12 rounded-full bg-gray-500" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[250px] bg-gray-500" />
+                <Skeleton className="h-4 w-[200px] bg-gray-500" />
+              </div>
+            </div>
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
 
